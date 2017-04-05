fffd090a7df21bf8367728be8cf9b3f1a812bdbf
meng0028 <robertyanming@gmail.com>
2017-04-05 14:32:57 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,148 +1,300 @@
-//ask the browser to support the latest programing language
+/*****************************************************************
+File: main.js
+Author: Yanming Meng
+Description: MAD9022 Midterm Project - GiftR
+Here is the sequence of logic for the app:
+- This is a Cordova App that help people save gift ideas
+- There are two pages, one shows people list, the other shows gift ideas.
+- All data is saved in localStorage 
+Version: 0.0.1
+Updated: March, 30 2017
+*****************************************************************/
 'use strict';
 var app = {
-    localStorageList: {
-        people: []
-    }
-    , init: function () {
-        document.addEventListener('deviceready', app.onDeviceReady);
-    }
-    , onDeviceReady: function () {
-        window.addEventListener('push', app.pageChanged);
-        //set time format to Canadian English
-        moment.locale('en-ca');
-        app.showPeoplePage();
-    }
-    , pageChanged: function () {
-        // load index.html
-        if (location.pathname.indexOf("index.html") != -1) {
-            app.showPeoplePage();
-            let addPersonBtn = document.getElementById("addPersonBtn");
-            let savePersonBtn = document.getElementById("savePersonBtn");
-            let closePersonBtn = document.getElementById("closePersonBtn");
-            // add click listener for each button
-            addPersonBtn.addEventListener("touchend", app.showPeoplePage.addPerson);
-            savePersonBtn.addEventListener("touchend", app.showPeoplePage.savePerson);
-            closePersonBtn.addEventListener("touchend", app.showPeoplePage.closePerson);
-        }
-        // load gifts.html
-        else {
-            app.showGiftsPage();
-            let addGiftBtn = document.getElementById("addGiftBtn");
-            let saveGiftBtn = document.getElementById("saveGiftBtn");
-            let closeGiftBtn = document.getElementById("closeGiftBtn");
-            let removeGiftBtn = document.getElementById("removeGiftBtn");
-            // add click listener for each button
-            addGiftBtn.addEventListener("touchend", app.showGiftsPage.addGift);
-            saveGiftBtn.addEventListener("touchend", app.showGiftsPage.saveGift);
-            closeGiftBtn.addEventListener("touchend", app.showGiftsPage.closeGift);
-            removeGiftBtn.addEventListener("touchend", app.showGiftsPage.removeGift);
-        }
-    }
-    , showPeoplePage: function () {
-        peopleId: 0
-        , printPeopleList: function (contactList = []) {
-            let peopleList = document.getElementById("contact-list");
-            // clear people html
-            peopleList.innerHTML = "";
-            app.localStorageList = JSON.parse(localStorage.getItem("giftr-meng0028"));
-            app.localStorageList.people.sortByBirthDate;
-            peopleList.forEach(function (people) {
-                let li = document.createElement("li");
-                li.className = "table-view-cell";
-                let dob = moment(people.dob).set("year", moment().year());
-                li.innerHTML = "".concat('<span class="name"><a href="#personModal">', people.name, '</a></span>', '<a class="navigate-right pull-right" href="gifts.html">', '<span ' + (dob < moment() ? 'class="dob"' : "") + '>', dob.format("MMMM Do"), '</span>', '</a>');
-                //adds Event listeners
-                li.querySelector("span a").addEventListener("touchend", () => app.showPeoplePagePage.addPerson(people.id));
-                li.querySelector("a.navigate-right.pull-right").addEventListener("touchend", () => app.showGiftsPage.peopleId = people.id);
-                //adds item to the list
-                document.getElementById("contact-list").appendChild(li);
-            });
-        }
-    }
-    , addPerson: function (peopleId) {
-        if (peopleId) {
-            let people = app.localData.getPersonById(peopleId);
-            if (people) {
-                this.peopleId = peopleId;
-                document.getElementById("person_name").value = people.name;
-                document.getElementById("person_dob").value = moment(people.dob).format("YYYY-MM-DD");
-                document.querySelector("#personModal header h1").textContent = "Edit: " + people.name;
-            }
-        }
-    }
-    , savePerson: function () {
-        let name = document.getElementById("contact_name").value.trim();
-        let dob = document.getElementById("contact_dob").value.trim();
-        if (name && dob) {
-            localStorage.setItem( 'giftr-meng0028' , JSON.stringify( lsData ) ); 
-            app.showPeoplePage.printPeopleList(app.localData.getLocalStorageData().people);
-            app.showPeoplePage.closePerson();
-        }
-    }
-    , closePerson: function () {
-        app.contactsPage.closePerson();
-        document.getElementById("close_contact").dispatchEvent(new CustomEvent("touchend", {
-            bubbles: true
-            , cancelable: true
-        }));
-    }
-    , showGiftsPage: {
-        giftId: 0
-        , personId: 0
-        , printGiftsList: function (giftList = []) {
-            let giftsList = document.getElementById("gift-list");
-            giftsList.innerHTML = "";
-            giftsList.forEach(function (gift) {
-                let url = /^(http|https):\/\//.test(gift.url) ? "" : "http://" + gift.url;
-                let li = document.createElement("li");
-                li.className = "table-view-cell media";
-                li.innerHTML = "".concat('<span class="pull-right icon icon-trash midline"></span>', '<div class="media-body">', gift.idea, (gift.store ? '<p>at ' + gift.store + '</p>' : ""), (gift.url ? '<a href="' + url + '" target="_blank" >' + gift.url + '</a>' : ""), (gift.cost ? '<p>' + (/^\$/.test(gift.cost) ? "" : "$") + gift.cost + '</p>' : ""), '</div>');
-                li.querySelector("span").addEventListener("click", () => app.showGiftsPage.removeGift(gift.id));
-                document.getElementById("gift-list").appendChild(li);
-            });
-        }
-    }
-    , addGift: function () {
-        let contact = app.localData.getPersonById(app.giftsPage.contactId);
-        if (contact) {
-            app.giftsPage.drawGiftsList(contact.ideas);
-            document.querySelector("#giftModal div p").textContent = "New idea for " + people.name;
-            document.querySelector(".content h5").textContent = "Ideas for " + people.name;
-        }
-    }
-    , saveGift: function () {
-        let idea = document.getElementById("gift_idea").value.trim();
-        let store = document.getElementById("gift_store").value.trim();
-        let url = document.getElementById("gift_url").value.trim();
-        let cost = document.getElementById("gift_cost").value.trim();
-        if (idea) {
-            localStorage.setItem( 'giftr-meng0028' , JSON.stringify( lsData ) ); 
-            app.giftsPage.printGiftsList(app.localData.getPersonById(app.giftsPage.peopleId).ideas);
-            app.giftsPage.closeGift();
-        }
-    }
-    , closeGift: function () {
-        app.giftsPage.cleanModal();
-        document.getElementById("close_gift").dispatchEvent(new CustomEvent("touchend", {
-            bubbles: true
-            , cancelable: true
-        }));
-    }
-    , removeGift: function (id) {
-        localStorage.removeItem( 'giftr-meng0028' , JSON.stringify( lsData ) ); 
-        let contact = app.localData.getPersonById(app.showGiftsPage.contactId);
-        if (contact) {
-            app.showGiftsPage.printGiftsList(contact.ideas);
-        }
-    }
-    , sortByBirthDate: function (people) {
-        return people.sort(function (a, b) {
-            let ma = moment(a.dob).set("year", moment().year());
-            let mb = moment(b.dob).set("year", moment().year());
-            return ma < mb ? -1 : ma > mb ? 1 : 0;
-        });
-    }
+	// declare variales
+	localStorageList: {
+		people: []
+	}
+	, currrentPerson: null
+		// main function
+		
+	, init: function () {
+		document.addEventListener('deviceready', app.onDeviceReady);
+	}
+	, onDeviceReady: function () {
+			console.dir("enter onDeviceReady");
+			window.addEventListener('push', app.pageChanged);
+			var addButton = document.getElementById("addPersonButton");
+			addButton.addEventListener("touchstart", function (ev) {
+				app.currrentPerson = 0;
+				document.getElementById("name").value = "";
+				document.getElementById("dateOfBirth").value = "";
+			});
+			// add event listener to person modal buttons
+			let savePersonButton = document.getElementById("savePersonButton");
+			savePersonButton.addEventListener("touchend", app.savePerson);
+			let cancelPersonButton = document.getElementById("cancelPersonButton");
+			cancelPersonButton.addEventListener("touchend", app.cancelPerson);
+			app.showIndexPage();
+		}
+		// page change function
+		
+	, pageChanged: function () {
+			let contentDiv = document.querySelector(".content");
+			let id = contentDiv.id;
+			switch (id) {
+			case "index":
+				// load index page
+				app.showIndexPage();
+				var addButton = document.getElementById("addPersonButton");
+				addButton.addEventListener("touchstart", function (ev) {
+					app.currrentPerson = 0;
+					document.getElementById("name").value = "";
+					document.getElementById("dateOfBirth").value = "";
+				});
+				// add event listener to person modal buttons
+			let savePersonButton = document.getElementById("savePersonButton");
+			savePersonButton.addEventListener("touchend", app.savePerson);
+			let cancelPersonButton = document.getElementById("cancelPersonButton");
+			cancelPersonButton.addEventListener("touchend", app.cancelPerson);
+				break;
+			case "gifts":
+				//load gifts page
+				app.showGiftPage();
+				var addGiftButton = document.getElementById("addGiftButton");
+				addGiftButton.addEventListener("touchstart", function (ev) {
+					document.getElementById("idea").value = "";
+					document.getElementById("store").value = "";
+					document.getElementById("url").value = "";
+					document.getElementById("cost").value = "";
+				});
+				// add event listener to gift modal buttons
+				let saveGiftButton = document.getElementById("saveGiftButton");
+				saveGiftButton.addEventListener("touchend", app.saveGift);
+				let cancelGiftButton = document.getElementById("cancelGiftButton");
+				cancelGiftButton.addEventListener("touchend", app.cancelGift);
+				break;
+			default:
+				app.showIndexPage();
+			}
+		}
+		// local storage functions
+		
+	, saveLocalStorage: function () {
+		localStorage.setItem("giftr-meng0028", JSON.stringify(people));
+	}
+	, getLocalStorage: function () {
+			if (!localStorage.getItem("giftr-meng0028")) {}
+			else {
+				people = JSON.parse(localStorage.getItem("giftr-meng0028"));
+			}
+		}
+		// index page functions
+		
+	, savePerson: function () {
+		if (app.currrentPerson == 0) {
+			var tempArray = document.getElementById("dateOfBirth").value.split("-");
+			var monthDayString = tempArray[1] + tempArray[2];
+			let temPerson = {
+				id: Date.now()
+				, fullname: document.getElementById("name").value
+				, dateOfBirth: document.getElementById("dateOfBirth").value
+				, monthDay: parseInt(monthDayString)
+				, ideas: []
+			};
+			// if both full name and DOB entered, save to local storage
+			if (temPerson.fullname && temPerson.dateOfBirth) {
+				app.localStorageList.people.push(temPerson);
+			}
+		}
+		else {
+			var indexCurrentLiClicked = -1;
+			for (var i = 0, len = app.localStorageList.people.length; i < len; i++) {
+				if (app.currrentPerson == app.localStorageList.people[i].id) {
+					indexCurrentLiClicked = i;
+				}
+			}
+			app.localStorageList.people[indexCurrentLiClicked].fullname = document.getElementById("name").value;
+			app.localStorageList.people[indexCurrentLiClicked].dateOfBirth = document.getElementById("dateOfBirth").value;
+			var tempArray = document.getElementById("dateOfBirth").value.split("-");
+			var monthDayString = tempArray[1] + tempArray[2];
+			app.localStorageList.people[indexCurrentLiClicked].monthDay = parseInt(monthDayString);
+		}
+		localStorage.setItem("giftr-meng0028", JSON.stringify(app.localStorageList));
+		var touchEndEvent = new CustomEvent("touchend", {
+			bubbles: true
+		});
+		var closePersonModal = document.getElementById("closePersonModal");
+		closePersonModal.dispatchEvent(touchEndEvent);
+		app.showIndexPage();
+	}
+	, cancelPerson: function () {
+		var touchEndEvent = new CustomEvent("touchend", {
+			bubbles: true
+		});
+		var closePersonModal = document.getElementById("closePersonModal");
+		closePersonModal.dispatchEvent(touchEndEvent);
+	}
+	, showIndexPage: function () {
+			let list = document.querySelector('#contact-list');
+			list.innerHTML = "";
+			app.localStorageList = JSON.parse(localStorage.getItem("giftr-meng0028"));
+			if (!app.localStorageList) {
+				app.localStorageList = {
+					people: []
+				};
+			}
+			else {
+				app.localStorageList.people.sort(function (a, b) {
+					return a.monthDay - b.monthDay;
+				});
+			}
+			app.localStorageList.people.forEach(function (person) {
+				let li = document.createElement("li");
+				li.className = "table-view-cell";
+				/
+				var dateObj = new Date();
+				var monthCurrent = (dateObj.getMonth() + 1).toString();
+				var dayCurrent = dateObj.getDate().toString();
+				var monthDayCurrentNum = -1;
+				if (dateObj.getDate() < 10) {
+					monthDayCurrentNum = parseInt(monthCurrent) * 100 + parseInt(dayCurrent);
+				}
+				else {
+					monthDayCurrentNum = parseInt(monthCurrent + dayCurrent);
+				}
+				if (person.monthDay < monthDayCurrentNum) {
+					li.classList.add("grey");
+				}
+				li.setAttribute("data-id", person.id);
+				let span_name = document.createElement("span");
+				span_name.className = "name";
+				let a_modal = document.createElement("a");
+				a_modal.href = "#personModal";
+				a_modal.innerHTML = person.fullname;
+				span_name.appendChild(a_modal);
+				a_modal.addEventListener("touchstart", function (ev) {
+					var anchorTemp = ev.currentTarget;
+					app.currrentPerson = anchorTemp.parentNode.parentNode.getAttribute("data-id");
+					document.getElementById("name").value = person.fullname;
+					document.getElementById("dateOfBirth").value = person.dateOfBirth;
+				});
+				let a_chevron = document.createElement("a");
+				a_chevron.href = "gifts.html";
+				a_chevron.className = "navigate-right pull-right goToGiftPage";
+				a_chevron.addEventListener("touchstart", function (ev) {
+					var anchorTemp = ev.currentTarget;
+					app.currrentPerson = anchorTemp.parentNode.getAttribute("data-id");
+				});
+				let span_dob = document.createElement("span");
+				span_dob.className = "dob";
+				let dateToMoment = moment(person.dateOfBirth).format("MMMM D");
+				span_dob.innerHTML = dateToMoment;
+				a_chevron.appendChild(span_dob);
+				li.appendChild(span_name);
+				li.appendChild(a_chevron);
+				list.appendChild(li);
+			});
+		}
+		//gifts page functions
+		
+	, saveGift: function () {
+		var indexTemp = -1;
+		for (var i = 0, len = app.localStorageList.people.length; i < len; i++) {
+			if (app.currrentPerson == app.localStorageList.people[i].id) {
+				indexTemp = i;
+				break;
+			}
+		}
+		let ideaEachTemp = {
+			id: Date.now()
+			, idea: document.getElementById("idea").value
+			, at: document.getElementById("store").value
+			, url: document.getElementById("url").value
+			, cost: document.getElementById("cost").value
+		};
+		if (document.getElementById("idea").value) {
+			app.localStorageList.people[indexTemp].ideas.push(ideaEachTemp);
+			localStorage.setItem("giftr-meng0028", JSON.stringify(app.localStorageList));
+		}
+		var touchEndEvent = new CustomEvent("touchend", {
+			bubbles: true
+		});
+		var closeGiftModal = document.getElementById("closeGiftModal");
+		closeGiftModal.dispatchEvent(touchEndEvent);
+		app.showGiftPage();
+	}
+	, cancelGift: function () {
+		var touchEndEvent = new CustomEvent("touchend", {
+			bubbles: true
+		});
+		var closeGiftModal = document.getElementById("closeGiftModal");
+		closeGiftModal.dispatchEvent(touchEndEvent);
+	}
+	, showGiftPage: function () {
+		let list = document.getElementById("gift-list");
+		list.innerHTML = "";
+		app.localStorageList = JSON.parse(localStorage.getItem("giftr-meng0028"));
+		if (!app.localStorageList) {
+			app.localStorageList = {
+				people: []
+			};
+		}
+		var indexTemp = -1;
+		for (var i = 0, len = app.localStorageList.people.length; i < len; i++) {
+			if (app.currrentPerson == app.localStorageList.people[i].id) {
+				indexTemp = i;
+				break;
+			}
+		}
+		let giftHeading = document.getElementById("gift-heading");
+		giftHeading.textContent = "Gift idea for " + app.localStorageList.people[indexTemp].fullname;
+		let modalHeading = document.getElementById("modal-heading");
+		modalHeading.textContent = "Gift idea for " + app.localStorageList.people[indexTemp].fullname;
+		for (var i = 0, len = app.localStorageList.people[indexTemp].ideas.length; i < len; i++) {
+			let li = document.createElement("li");
+			li.className = "table-view-cell media";
+			let span = document.createElement("span");
+			span.className = "pull-right icon icon-trash midline";
+			span.setAttribute("gift-idea-id", app.localStorageList.people[indexTemp].ideas[i].id);
+			span.addEventListener("touchend", function (ev) {
+				let deleteIcon = ev.currentTarget;
+				let idForDelele = deleteIcon.getAttribute("gift-idea-id");
+				let len = app.localStorageList.people[indexTemp].ideas.length;
+				let indexDeleteItem = -1;
+				for (let i = 0; i < len; i++) {
+					if (idForDelele == app.localStorageList.people[indexTemp].ideas[i].id) {
+						indexDeleteItem = i;
+						break;
+					}
+				}
+				if (indexDeleteItem > -1) {
+					app.localStorageList.people[indexTemp].ideas.splice(indexDeleteItem, 1);
+					app.saveLocalStorage();
+					ev.currentTarget.parentElement.parentElement.removeChild(ev.currentTarget.parentElement);
+				}
+			});
+			let div = document.createElement("div");
+			div.className = "media-body";
+			div.textContent = app.localStorageList.people[indexTemp].ideas[i].idea;
+			let pstore = document.createElement("p");
+			pstore.textContent = app.localStorageList.people[indexTemp].ideas[i].at;
+			let purl = document.createElement("p");
+			let a = document.createElement("a");
+			a.href = "http://" + app.localStorageList.people[indexTemp].ideas[i].url;
+			a.target = "_blank";
+			a.innerHTML = app.localStorageList.people[indexTemp].ideas[i].url;
+			purl.appendChild(a);
+			let pcost = document.createElement("p");
+			pcost.innerHTML = app.localStorageList.people[indexTemp].ideas[i].cost;
+			div.appendChild(pstore);
+			div.appendChild(purl);
+			div.appendChild(pcost);
+			li.appendChild(span);
+			li.appendChild(div);
+			list.appendChild(li);
+		}
+	}
 };
 app.init();
 
